d77fbf6923a3aefdb3d64f8156544e05909a986e
Gaurav <gaurav1567@gmail.com>
2024-11-07 22:33:09 +0530

    testing done

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,10 @@
 export default (): any => ({
   database: {
     type: 'postgres',
-    host: process.env.POSTGRES_HOST || 'localhost',
-    username: process.env.POSTGRES_USERNAME || 'postgres',
-    password: process.env.POSTGRES_PASSWORD || 'hyperhire_backend',
-    database: process.env.POSTGRES_DATABASE || 'postgres',
+    host: process.env.POSTGRES_HOST,
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
     entities: [`${__dirname}/../**/*.entity.{js,ts}`],
     synchronize: true,
     ssl: process.env.POSTGRES_SSL,
